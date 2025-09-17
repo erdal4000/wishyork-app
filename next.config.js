7fffe2a6d9eeada63576368294ffc94863b00d06
HerhangiBirŞey (HerhangiBirŞey) <erdal400@gmail.com>
2025-09-17 13:53:26 +0000

    iki ayrı siteden ürün yüklemek istedim verdiği hata şöyle: Unhandled Run

--- next.config.js
+++ next.config.js
@@ -7,21 +7,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
     ],
   },
